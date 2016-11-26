529e3832faa3086fe4844f53d2b87eee58dcaa6f
Bill Gottsch <billgottsch@gmail.com>
2016-11-26 10:33:51 -0600

    some styling, fonts

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ $(function() {
       console.log(data);
       var netflixGroup = $('<div class="netflixGroup"></div>');
       var netflixSubGroup = $('<div class="netflixSubGroup"></div>');
-        netflixSubGroup.append($('<h2 class="movieName"></h2>').text(data.Title));
+        netflixSubGroup.append($('<h2 class="movieName"></h2>' + '<br>').text(data.Title));
+        netflixSubGroup.append($('<img class="moviePoster">').attr('src', data.Poster));
         netflixSubGroup.append($('<p class="movieYear"></p>').text(data.Year));
-        netflixSubGroup.append($('<p class="movieRating"></p>').text(data.Rated));
+        netflixSubGroup.append($('<p class="movieRating"></p>').text('Rated: ' + data.Rated));
         netflixSubGroup.append($('<p class="movieActors"></p>').text(data.Actors));
         netflixSubGroup.append($('<p class="movieRating"></p>').text(data.Genre));
         netflixSubGroup.append($('<p class="moviePlot"></p>').text(data.Plot));
         netflixSubGroup.append($('<p class="movieScore"></p>').text(data.imdbRating + '/10'));
-        netflixSubGroup.append($('<img>').attr('src', data.Poster));
 
 
 
